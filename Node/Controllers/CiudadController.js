f007af5928048793cac8bf75aeb936f0e66a98b5
unknown <estefaniaotalvaro1@gmail.com>
2025-10-27 13:48:33 -0500

    Agrego CiudadController.js

---
+++ Node/Controllers/CiudadController.js
@@ -0,0 +1,54 @@
+import CiudadService from "../Services/CiudadService.js";
+
+// Obtener todos los responsables
+export const getAllCiudades = async (req, res) => {
+    try {
+
+        const ciudad = await CiudadService.getAll()
+        res.status(200).json(ciudad)
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+// Obtener una ciudad por ID
+export const getCiudad = async (req, res) => {
+    try {
+        const ciudad = await CiudadService.getById(req.params.id)
+        res.status(200).json(ciudad)
+
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+// Crear una nueva ciudad
+export const createCiudad = async (req, res) => {
+    try {
+
+        const newCiudad = await CiudadService.create(req.body)
+        res.status(201).json({message: 'Ciudad creada', newCiudad})
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+// Actualizar una ciudad existente
+export const updateCiudad = async (req, res) => {
+    try {
+        await CiudadService.update(req.params.id, req.body)
+        res.status(200).json({ message: 'Ciudad actualizada' })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
+
+// Eliminar una ciudad
+export const deleteCiudad = async (req, res) => {
+    try {
+        await CiudadService.delete(req.params.id)
+        res.status(204).send()
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
