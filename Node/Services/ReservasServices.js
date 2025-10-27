cab1238cda1ccaa803269937cfdb27b0f5aaa694
unknown <estefaniaotalvaro1@gmail.com>
2025-10-27 14:24:49 -0500

    Agrego ReservasServices.js

---
+++ Node/Services/ReservasServices.js
@@ -0,0 +1,33 @@
+import ReservasModel from "../Models/ReservasModel.js";
+class ReservasServices {
+    async getAll() {
+        return await ReservasModel.findAll()
+
+    }
+    async getById(id) {
+        const Reservas = await ReservasModel.findByPk(id)
+        if(!Reservas) throw new Error("Reserva no encontrada")
+        return Reservas
+    }
+    async create(data){
+        return await ReservasModel.create(data)
+    }
+    async update(id, data) {
+        const result = await ReservasModel.update(data, { where: { id_Reserva: id } });
+
+        if (result[0] === 0) {
+            const Reservas = await ReservasModel.findByPk(id);
+            if (!Reservas) throw new Error("Reserva no encontrada");
+            throw new Error("No hubo cambios en la reserva (los datos son iguales)");
+            }
+
+  return true;
+}
+
+    async delete(id){
+        const deleted = await ReservasModel.destroy({where:{id_Reserva: id}})
+        if (!deleted) throw new Error("Reservas no encontrada")
+        return true
+    }
+}
+export default new ReservasServices
