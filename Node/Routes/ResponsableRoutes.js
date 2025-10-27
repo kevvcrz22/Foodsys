39aa7c6b9cf53b6736f1f1556de472d1b5df1802
unknown <estefaniaotalvaro1@gmail.com>
2025-10-27 14:33:44 -0500

    Agrego ResponsableRoutes.js

---
+++ Node/Routes/ResponsableRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getAllResponsables, getResponsable, createResponsable, updateResponsable, deleteResponsable } from "../Controllers/ResponsableController.js";
+
+const router = express.Router();
+
+router.get('/', getAllResponsables);
+router.get('/:id', getResponsable);
+router.post('/', createResponsable);
+router.put('/:id', updateResponsable);
+router.delete('/:id', deleteResponsable);
+
+export default router;
