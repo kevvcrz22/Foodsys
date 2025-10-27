e2b05365ac0e2d110761f003bfb1f27cba7b3e9d
30062004-san <santigrijalba007@gmail.com>
2025-10-27 10:06:31 -0500

    creacion de controller, model, routes, service, y modificacion en las rutas de app con su respectiva tabla de responsables, ciudad, y centro

---
+++ Node/Models/CiudadModel.js
@@ -0,0 +1,10 @@
+import db from "../database/db.js";
+import { DataTypes } from "sequelize";
+
+const CiudadModel = db.define('ciudad', {
+    Id_Ciudad: {type: DataTypes.NUMBER, primaryKey: true, autoIncrement: true},
+    Nom_Ciudad: {type: DataTypes.STRING},
+}, {
+    freezeTableName: true
+})
+export default CiudadModel
