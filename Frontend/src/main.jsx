10b33996c74edf82ff7e198dfab2464a6d432383
santofimio07 <diegosantofimio328@gmail.com>
2025-11-25 08:35:57 -0500

    agregue lineas a la main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,10 +9,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import CrudFichas from './Fichas/CrudFichas.jsx'
 import FichasForm from './Fichas/FichasForm.jsx'
+import CrudPrograma from './Programa/CrudPrograma.jsx'
+import ProgramaForm from './Programa/ProgramaForm.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <CrudFichas/>
+    <CrudPrograma/>
+    
   </React.StrictMode>,
 )
