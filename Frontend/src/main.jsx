bf198622af36d942f4ca2c7a9d12cb57c6af51c1
kevin fierro <fierrokkevin21@gmail.com>
2025-11-29 16:41:14 -0500

    Arregle las rutas entre ficha y programa para conectar

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -10,11 +10,10 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import CrudUsuarios from './Usuarios/CrudUsuarios.jsx'
 import UsuariosForm from './Usuarios/UsuariosForm.jsx'
-import 'bootstrap/dist/js/bootstrap.min.js'
 import CrudFichas from './Fichas/CrudFichas.jsx'
 import FichasForm from './Fichas/FichasForm.jsx'
-import CrudPrograma from './Programa/CrudPrograma.jsx'
-import ProgramaForm from './Programa/ProgramaForm.jsx'
+import CrudPrograma from './Programas/CrudPrograma.jsx'
+import ProgramaForm from './Programas/ProgramaForm.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
