0234c36df7c0ba52cab195a578730b5852680e34
kevin fierro <fierrokkevin21@gmail.com>
2025-12-16 07:34:57 -0500

    Creacion de la carpeta de las paginas, esta debe haber las carpetas correspondientes de (Login, Admin, Supervisor, Aprendiz)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Login from "./Paginas/Login/Login.jsx";
+import CrudUsuarios from "./Usuarios/CrudUsuarios.jsx";
 
 function App() {
-  
+  const [usuarioLogeado, setUsuarioLogeado] = useState(null);
 
+  // SI NO ESTÁ LOGEADO → login
+  if (!usuarioLogeado) {
+    return <Login onLogin={setUsuarioLogeado} />;
+  }
+
+  // SI EL USUARIO ES ADMINISTRADOR
+  if (usuarioLogeado?.Tip_Usuario === "Administrador") {
+    return <CrudUsuarios />;
+  }
+
+  // PARA OTROS TIPOS DE USUARIO
   return (
-    <>
-      
-    </>
-  )
+    <h2 style={{ textAlign: "center", marginTop: "50px" }}>
+      Bienvenido {usuarioLogeado?.Nom_Usuario}
+    </h2>
+  );
 }
 
-export default App
+export default App;
