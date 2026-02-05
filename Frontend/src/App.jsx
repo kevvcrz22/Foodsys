d157859693dedfe8be9295f32e89a3fe51ac6639
unknown <estefaniaotalvaro1@gmail.com>
2026-02-05 11:02:38 -0500

    Hacer el NavBar, Rutas de las tablas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./Paginas/Login/Login.jsx";
-import CrudUsuarios from "./Usuarios/CrudUsuarios.jsx";
 import Chatbot from "./components/Chatbot.jsx";
+
+import CrudUsuarios from "./Usuarios/CrudUsuarios.jsx";
+import CrudFichas from "./Fichas/CrudFichas.jsx";
+import CrudPrograma from "./Programas/CrudPrograma.jsx";
+import CrudReservas from "./Reservas/CrudReservas.jsx";
+import NavBar from "./navBar.jsx";
 
 function App() {
   const [usuarioLogeado, setUsuarioLogeado] = useState(null);
@@ -10,18 +17,24 @@ function App() {
     <>
       {/* CHATBOT SIEMPRE VISIBLE */}
       <Chatbot />
+      <NavBar />
+      <Routes>
+        {/* RUTA PÚBLICA */}
+        <Route path="/" element={<Login onLogin={setUsuarioLogeado} />} />
 
-      {/* CONTENIDO PRINCIPAL */}
-      {!usuarioLogeado && <Login onLogin={setUsuarioLogeado} />}
+        {/* RUTAS PÚBLICAS (SIN LOGIN) */}
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/usuarios" element={<CrudUsuarios />} />
+        <Route path="/fichas" element={<CrudFichas />} />
+        <Route path="/programas" element={<CrudPrograma />} />
+        <Route path="/reservas" element={<CrudReservas />} />
+      </Routes>
 
-      {usuarioLogeado?.Tip_Usuario === "Administrador" && <CrudUsuarios />}
-
-      {usuarioLogeado &&
-        usuarioLogeado?.Tip_Usuario !== "Administrador" && (
-          <h2 style={{ textAlign: "center", marginTop: "50px" }}>
-            Bienvenido {usuarioLogeado.Nom_Usuario}
-          </h2>
-        )}
+      {usuarioLogeado && (
+        <h2 style={{ textAlign: "center", marginTop: "50px" }}>
+          Bienvenido {usuarioLogeado.Nom_Usuario}
+        </h2>
+      )}
     </>
   );
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,28 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-import CrudUsuarios from './Usuarios/CrudUsuarios.jsx'
-import UsuariosForm from './Usuarios/UsuariosForm.jsx'
-import CrudFichas from './Fichas/CrudFichas.jsx'
-import FichasForm from './Fichas/FichasForm.jsx'
-import CrudPrograma from './Programas/CrudPrograma.jsx'
-import ProgramaForm from './Programas/ProgramaForm.jsx'
-import CrudReservas from './Reservas/CrudReservas.jsx'
-import ReservasForm from './Reservas/ReservaForm.jsx'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-    <CrudUsuarios/>
-    <CrudFichas/>
-    <CrudPrograma/>
-    <CrudReservas/>
-  </StrictMode>
+
+<BrowserRouter>
+   <App/>
+</BrowserRouter>
+
 )
