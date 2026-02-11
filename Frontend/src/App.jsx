109a118fb9e186d4b2ef6760592ae507159dce7b
kevin fierro <fierrokkevin21@gmail.com>
2026-02-11 14:36:55 -0500

    Parte de Footer y Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,26 +9,27 @@ import CrudFichas from "./Tablas/Fichas/CrudFichas.jsx";
 import CrudPrograma from "./Tablas/Programas/CrudPrograma.jsx";
 import CrudReservas from "./Tablas/Reservas/CrudReservas.jsx";
 import NavBar from "./Paginas/navBar.jsx";
+import Footer from "./Paginas/Footer.jsx";
 
 function App() {
   const [usuarioLogeado, setUsuarioLogeado] = useState(null);
 
   return (
     <>
-      {/* CHATBOT SIEMPRE VISIBLE */}
       <Chatbot />
       <NavBar />
       <Routes>
-        {/* RUTA PÚBLICA */}
+
         <Route path="/" element={<Login onLogin={setUsuarioLogeado} />} />
 
-        {/* RUTAS PÚBLICAS (SIN LOGIN) */}
         <Route path="/Login" element={<Login/>} />
         <Route path="/usuarios" element={<CrudUsuarios />} />
         <Route path="/fichas" element={<CrudFichas />} />
         <Route path="/programas" element={<CrudPrograma />} />
         <Route path="/reservas" element={<CrudReservas />} />
       </Routes>
+
+      <Footer />
 
       {usuarioLogeado && (
         <h2 style={{ textAlign: "center", marginTop: "50px" }}>
